785cbc97f36d872d13aa7bbaff37982e88e03443
Sadiqvayani <62079259+Sadiqvayani@users.noreply.github.com>
2020-03-16 02:03:06 +0500

    Sample Git
    Author: Mr Vayani

---
+++ src/Aph.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Aph(){
+
+    return (
+     <div>
+    <input type="checkbox" ></input>
+    <p>1.</p>
+    <input type="checkbox" ></input>
+    <p>2</p>
+    <input type="checkbox" ></input>
+    <p>3.</p>
+
+
+
+     </div>
+
+
+    );
+}
+export default Aph;
---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="http://placekitten.com/g/200/300" className="App-logo" alt="logo" />
+        <h1>
+         broadway, hello dear, Askari
+        </h1>
+        <a
+        
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
